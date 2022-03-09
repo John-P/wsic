5484e6c068adc405a64bb0f0c2efca2d6c029884
John Pocock <John-P@users.noreply.github.com>
2022-03-09 02:13:21 +0000

    :wrench: Add __all__ To Top-Level __init__

--- wsic/__init__.py
+++ wsic/__init__.py
@@ -5,3 +5,11 @@ __email__ = "j.c.pocock@warwick.ac.uk"
 __version__ = "0.1.0"
 
 from . import codecs, magic, readers, types, writers
+
+__all__ = [
+    "codecs",
+    "magic",
+    "readers",
+    "types",
+    "writers",
+]
