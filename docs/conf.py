98388996b0a132da3d49cdd8fff35f6eaf414517
John Pocock <John-P@users.noreply.github.com>
2022-08-12 16:47:53 +0100

    :memo: Remove Unused Config Option & Format RST Indents

--- docs/conf.py
+++ docs/conf.py
@@ -124,7 +124,6 @@ latex_elements = {
     # Latex figure (float) alignment
     #
     # 'figure_align': 'htbp',
-    "extrapackages": r"\usepackage{svg}",
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
