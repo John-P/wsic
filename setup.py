bb067e515d686bb79ed928dba514c8be1876ad32
John Pocock <John-P@users.noreply.github.com>
2022-10-03 11:01:44 +0100

    :bookmark: Bump version: 0.5.1 → 0.6.0

--- setup.py
+++ setup.py
@@ -106,6 +106,6 @@ setup(
     tests_require=test_requirements,
     extras_require=extra_requirements,
     url="https://github.com/john-p/wsic",
-    version="0.5.1",
+    version="0.6.0",
     zip_safe=False,
 )
