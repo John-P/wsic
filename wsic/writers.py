26788ddb3159a3c1516dae7d4d969933993c5f8e
John Pocock <John-P@users.noreply.github.com>
2022-10-21 19:31:44 +0100

    :bug: Minor Bug Fixes (#40)
    * :bug: Ensure cv2 Arg Is an int Tuple
    
    * :sparkles: Add TIFF Writer For CLI Based On Ext
    
    * :bug: Add Missing **kwargs
    
    * :children_crossing: Change TIFFWriter OME Default To False
    
    * :pencil2: Typo Fix transcribed -> transcoded
    
    * :recycle: Move CLI Tests To Seperate Module
    
    * :bug: Fix Typo in Class For Transcoding From DICOM
    
    * :white_check_mark: Add CLI Tests

--- wsic/writers.py
+++ wsic/writers.py
@@ -318,6 +318,7 @@ class JP2Writer(Writer):
         pyramid_downsamples: Optional[List[int]] = None,
         overwrite: bool = False,
         verbose: bool = False,
+        **kwargs,
     ) -> None:
         if codec != "jpeg2000":
             warn_unused(codec)
@@ -487,7 +488,7 @@ class TIFFWriter(Writer):
         overwrite: bool = False,
         verbose: bool = False,
         *,
-        ome: bool = True,
+        ome: bool = False,
     ) -> None:
         if dtype is not np.uint8:
             warn_unused(dtype)
