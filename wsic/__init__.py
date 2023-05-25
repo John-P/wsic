0677935e189d16f0e1baa9146d2112135f67babf
John Pocock <John-P@users.noreply.github.com>
2023-05-25 13:25:30 +0100

    :bookmark: Bump version: 0.8.2 → 0.9.0

--- wsic/__init__.py
+++ wsic/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """John Pocock"""
 __email__ = "j.c.pocock@warwick.ac.uk"
-__version__ = "0.8.2"
+__version__ = "0.9.0"
 
 from . import codecs, magic, metadata, multiproc, readers, typedefs, writers
 
